3aa42a12c51eb73faa47b30cf13fa0d5c9fc6dea
Dragon1573 <Legend_1949@126.com>
2021-12-29 17:10:07 +0800

    重构：使用 Jupyter Notebook 存储

--- src/study_plan/algorithms/intro/quiz_278/provided.py
+++
@@ -1,9 +0,0 @@
-FIRST_BAD_VERSION = 0
-
-
-def initialize(version: int) -> None:
-    FIRST_BAD_VERSION = version
-
-
-def isBadVersion(version: int) -> bool:
-    return version >= FIRST_BAD_VERSION
